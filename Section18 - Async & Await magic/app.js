8d69c08c8f4ddc814bc4951e04ec85d0dd34a7f0
Jonathan Yates <carlock@Jonathans-MacBook-Pro.local>
2020-05-08 15:52:03 -0700

    finished await/async examples

--- Section18 - Async & Await magic/app.js
+++ Section18 - Async & Await magic/app.js
@@ -59,9 +59,70 @@ add(4, 5)
 
 // This way waits until promise is resolved, therefor we dont need .then
 // This is syntatic surgar just to make it easier to read.
-async function getPlanets() {
-    const res = await axios.get('https://swapi.dev/api/planets/')
-    console.log(res.data)
-}
+// async function getPlanets() {
+//     const res = await axios.get('https://swapi.dev/api/planets/')
+//     console.log(res.data)
+// }
 
-getPlanets()
+// getPlanets().catch((err) => {
+//     console.log('IN CATCH', err)
+// })
+
+
+// OR try, except, This works the same but more specific than catching all
+async function getPlanets() {
+    try {
+        const res = await axios.get('https://swapi.dev/api/planets/')
+        console.log(res.data)
+    } catch(e) {
+        console.log('IN CATCH', e)
+    }
+}
+// getPlanets()
+
+
+// REFACTORED BUTTON CODE TO USE ASYNC/AWAIT
+
+// const btn = document.querySelector('button')
+
+// const moveX = (element, amount, delay) => {
+//     return new Promise((resolve,reject) => {
+//         setTimeout(() => {
+//             const bodyBoundary = document.body.clientWidth;
+//             const elRight = element.getBoundingClientRect().right
+//             const currLeft = element.getBoundingClientRect().left
+//             if(elRight + amount > bodyBoundary) {
+//                 // passing params to use for err
+//                 reject({bodyBoundary, elRight, amount})
+//             } else {
+//                 element.style.transform = `translateX(${currLeft + amount}px)`
+//                 resolve()
+//                 }
+//         }, delay)
+//     })
+// }
+
+// async function animateRight(el, amt) {
+//     await moveX(el,amt,1000)
+//     await moveX(el,amt,1000)
+//     await moveX(el,amt,1000)
+//     await moveX(el,amt,1000)
+//     await moveX(el,amt,1000)
+//     await moveX(el,amt,1000)
+//     await moveX(el,amt,1000)
+//     await moveX(el,amt,1000)
+//     await moveX(el,amt,1000)
+//     await moveX(el,amt,1000)
+//     await moveX(el,amt,1000)
+// }
+// animateRight(btn, 100).catch((err) => {
+//     console.log('ALL DONE')
+//     // THIS ONLY WORKS AND GOES BACK IF CATCH IS EXECUTED
+//     animateRight(btn, -100);
+// })
+
+
+
+// PARALLEL Vs. SEQUENTIAL REQUESTS
+
+// https:
